fb27d889a4ec65dd342b155c94f3797bba1e07d9
architjain1996 <architjainudr.aj@gmail.com>
2021-09-28 18:31:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ function clickEventHandler()
 {
     var birthDate = document.getElementById("birthdateId").value;
     var luckyNumber = parseInt(document.getElementById("luckyNumberId").value);
-    birthDate=birthDate.replaceAll('-','');
-    var sum=0;
-    for(var i=0;i<birthDate.length;i++)
+    if(birthDate==undefined || birthDate==null || birthDate=="")
+    {
+        alert("Please enter birth date ");return false;
+    }
+    if(isNaN(luckyNumber))
+    {
+        alert("Please enter lucky number ");return false;
+    }
+    birthDate = birthDate.replaceAll('-','');
+    var sum = 0;
+    for(var i = 0; i < birthDate.length; i++)
     {
         sum += parseInt(birthDate[i]);
     }
-    sum=parseInt(sum);
-    if(sum%luckyNumber==0)
+    sum = parseInt(sum);
+    if(sum % luckyNumber == 0)
     {
         var Message="You have a lucky birthday";
     }
